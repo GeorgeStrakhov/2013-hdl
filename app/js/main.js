8a0abd71ed6ff744d745d8c180c1dbd9e6bec56f
George Strakhov <george.strakhov@gmail.com>
2013-12-10 10:39:08 +0800

    getting on

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,5 @@
 //blast off
 function initJS() {
-	registerListeners();
 	var dones = getParam('done');
 	var author = getParam('author');
 	var theme = getTheme(getParam('theme'));
@@ -13,9 +12,22 @@ function initJS() {
 
 //register event listeners
 function registerListeners() {
+	//create new
 	$('#generateHDL').on('click', function(){
 		var url = window.location.origin='?done='+encodeURIComponent($('#newHDLInput').val());
 		window.location.href=url;
+	});
+	//resize handler
+	$.viewportWidth = $(window).width();
+	$(window).smartresize(function() {
+		// Only run the resize code if the viewport width has changed.
+		// we ignore the viewport height as it will be constantly changing.
+		if($(window).width() == $.viewportWidth) {
+			return;
+		}
+		$.viewportWidth = $(window).width();
+		// console.log('resize to '+$.viewportWidth);
+		initJS();
 	});
 }
 
@@ -50,17 +62,43 @@ function linesToText(donesLines, theme) {
 	return donesText;
 }
 
+//adjust line heights depending on the font-size, calculated by slabtext
+function adjustLineHeights(){
+	$('.singleDone').each(function(k,v){
+		//ignore the first line
+		if(k===0) return;
+		//get this line's fontsize
+		var fontsize = parseInt($(this).find('span').first().css('font-size').split('px')[0], 10);
+		//get prev line's fontsize
+		var prevFontsize = parseInt($(this).prev().find('span').first().css('font-size').split('px')[0], 10);
+		//calculate and apply (mostly negative) top margin adj
+		var adj = calculateMarginTopAdjustment(fontsize, prevFontsize);
+		$(this).css('margin-top', adj+'px');
+	});
+}
+
+//calculate top margin adjustment based on this and prev lines fontsize
+function calculateMarginTopAdjustment(fontsize, prevFontsize) {
+	var optRatio = 5.8;
+	var topmargin = 0;
+	console.log(fontsize, prevFontsize);
+	topmargin = -1 * ((fontsize-prevFontsize)/optRatio);
+	return topmargin;
+}
+
 //render manifesto
 function manifestify(dones, theme) {
 	var donesLines = donesToLines(dones);
 	var donesText = linesToText(donesLines, theme);
-	$('.allDones').addClass(theme).append(donesText);
+	$('.allDones').addClass(theme).html(donesText);
 	$('.singleDone').slabText();
+	adjustLineHeights();
 	$('.goodyear').slabText();
 }
 
 //Let's get going
-$(document).ready(function($){
+$(window).bind('load', function(){
 	console.log('Blast off!!!');
+	registerListeners();
 	initJS();
 });
